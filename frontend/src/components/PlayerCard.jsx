8251a775522534d44c404340fef84c72cf139287
AsafKed <asafkedem97@gmail.com>
2023-05-28 19:23:17 +0200

    Add details to playercard

--- frontend/src/components/PlayerCard.jsx
+++ frontend/src/components/PlayerCard.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Modal, Card, CardContent, Typography, Box, CircularProgress } from '@mui/material';
+import { Modal, Card, CardContent, Typography, Box, CircularProgress, CardMedia } from '@mui/material';
+import ReactCountryFlag from "react-country-flag";
 
 const PlayerCard = ({ playerName, open, handleClose }) => {
     const [playerData, setPlayerData] = useState(null);
@@ -39,16 +40,51 @@ const PlayerCard = ({ playerName, open, handleClose }) => {
                         <CircularProgress />
                     ) : (
                         playerData && (
-                            <CardContent>
-                                <Typography variant="h5" component="div" id="player-card-title">
-                                    {playerData.name}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary" id="player-card-description">
-                                    Country: {playerData.country} <br/>
-                                    Rank: {playerData.rank} <br/>
-                                    {/* Add more player data here */}
-                                </Typography>
-                            </CardContent>
+                            <>
+                                <CardMedia
+                                    component="img"
+                                    height="300"
+                                    image={playerData.image_url}
+                                    alt={playerData.name}
+                                    sx={{ objectFit: 'contain' }}
+                                />
+                                <ReactCountryFlag
+                                    countryCode={playerData.country_code}
+                                    style={{
+                                        width: '2em',
+                                        height: '2em',
+                                        position: 'absolute',
+                                        top: '10px',
+                                        left: '10px',
+                                        zIndex: 1,
+                                    }}
+                                    svg
+                                />
+                                <CardContent>
+                                    <Typography variant="h5" component="div" id="player-card-title">
+                                        {playerData.name}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary" id="player-card-description">
+                                        Country: {playerData.country} <br/>
+                                        Rank: {playerData.rank} <br/>
+                                        Rank Level: {playerData.rank_level} <br/>
+                                        Status: {playerData.status} <br/>
+                                        Experience: {playerData.experience} <br/>
+                                        Play Style: {playerData.play_style} <br/>
+                                        Age: {playerData.age} <br/>
+                                        Height: {playerData.height} <br/>
+                                        Favorite Shot: {playerData.favorite_shot} <br/>
+                                        Hand: {playerData.hand} <br/>
+                                        Personality Tags: {playerData.personality_tags} <br/>
+                                        Personality Long: {playerData.personality_long} <br/>
+                                        Grass Advantage: {playerData.grass_advantage} <br/>
+                                        Career High Rank: {playerData.career_high_rank} <br/>
+                                        Years on Tour: {playerData.years_on_tour} <br/>
+                                        Coach: {playerData.coach} <br/>
+                                        Gender: {playerData.gender} <br/>
+                                    </Typography>
+                                </CardContent>
+                            </>
                         )
                     )}
                 </Card>

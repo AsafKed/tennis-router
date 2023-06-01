4a7598f179864f3f059e1748a71da6e7cc16ce62
AsafKed <asafkedem97@gmail.com>
2023-06-01 21:45:34 +0200

    Improve PlayerCard styling

--- frontend/src/components/PlayerCard.jsx
+++ frontend/src/components/PlayerCard.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Modal, Card, CardContent, Typography, Box, CircularProgress, CardMedia, Grid, Divider, Chip } from '@mui/material';
+import { Modal, Card, CardContent, Typography, Box, CircularProgress, CardMedia, Grid, Divider, Chip, Button, Collapse } from '@mui/material';
 import ReactCountryFlag from "react-country-flag";
-import { Star, SportsTennis, CalendarToday } from '@mui/icons-material';
+import { Star, SportsTennis, CalendarToday, ExpandMore } from '@mui/icons-material';
 
 const PlayerCard = ({ playerName, open, handleClose }) => {
     const [playerData, setPlayerData] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
 
     useEffect(() => {
         const fetchPlayerData = async () => {
@@ -41,8 +46,8 @@ const PlayerCard = ({ playerName, open, handleClose }) => {
                         <CircularProgress />
                     ) : (
                         playerData && (
-                            <Grid container>
-                                <Grid item xs={12} md={6}>
+                            <Card>
+                                <CardContent>
                                     <CardMedia
                                         component="img"
                                         height="300"
@@ -50,116 +55,86 @@ const PlayerCard = ({ playerName, open, handleClose }) => {
                                         alt={playerData.name}
                                         sx={{ objectFit: 'contain' }}
                                     />
+                                    <Box display="flex" alignContent={'center'}  alignItems={'center'}>
+                                        <Typography variant="h1" component="div">
+                                            {playerData.rank} 
+                                        </Typography>
+                                        <Typography variant="h4" component="div" id="player-card-title" padding={1}>
+                                            Ranking
+                                        </Typography>
+                                    </Box>
                                     <CardContent>
                                         <Typography variant="h4" component="div" id="player-card-title">
                                             {playerData.name}
                                         </Typography>
                                         <Grid container spacing={2}>
                                             <Grid item xs={12}>
-                                                <Card>
-                                                    <CardContent>
-                                                        <Typography variant="body2" color="text.secondary">
-                                                            <ReactCountryFlag
-                                                                countryCode={playerData.country_code}
-                                                                style={{
-                                                                    width: '1em',
-                                                                    height: '1em',
-                                                                }}
-                                                                svg
-                                                            /> {playerData.country}
-                                                        </Typography>
-                                                    </CardContent>
-                                                </Card>
-                                            </Grid>
-                                            <Grid item xs={12} sm={6}>
-                                                <Card>
-                                                    <CardContent>
-                                                        <Typography variant="h6" component="div">
-                                                            <Star /> Rank: {playerData.rank}
-                                                        </Typography>
-                                                        <Typography variant="body2" color="text.secondary">
-                                                            <SportsTennis /> Play Style: {playerData.play_style}
-                                                        </Typography>
-                                                    </CardContent>
-                                                </Card>
-                                            </Grid>
-                                            <Grid item xs={12} sm={6}>
-                                                <Card>
-                                                    <CardContent>
-                                                        <Typography variant="h6" component="div">
-                                                            <Star /> Career High Rank: {playerData.career_high_rank}
-                                                        </Typography>
-                                                        <Typography variant="body2" color="text.secondary">
-                                                            <CalendarToday /> Career High Year: {playerData.career_high_year}
-                                                        </Typography>
-                                                    </CardContent>
-                                                </Card>
+                                                <Typography variant="body1" color="text.secondary">
+                                                    <ReactCountryFlag
+                                                        countryCode={playerData.country_code}
+                                                        style={{
+                                                            width: '1em',
+                                                            height: '1em',
+                                                        }}
+                                                        svg
+                                                    /> {playerData.country} | {playerData.play_style} | Career High Rank: {playerData.career_high_rank} in {playerData.career_high_year}
+                                                </Typography>
                                             </Grid>
                                         </Grid>
-                                        <Typography variant="body2" color="text.secondary">
-                                            {playerData.personality_long}
-                                        </Typography>
-                                    </CardContent>
-                                </Grid>
-
-                                <Grid item xs={12} md={6}>
-                                    <CardContent>
-                                        <Typography variant="h5" component="div">
-                                            More info
-                                        </Typography>
-                                        <Grid container spacing={2}>
-                                            <Grid item xs={12} sm={6}>
-                                                <Card>
-                                                    <CardContent>
-                                                        <Typography variant="h6" component="div">
-                                                            <Star /> Rank: {playerData.rank}
-                                                        </Typography>
-                                                        <Typography variant="body2" color="text.secondary">
-                                                            <SportsTennis /> Play Style: {playerData.play_style}
-                                                        </Typography>
-                                                    </CardContent>
-                                                </Card>
-                                            </Grid>
-                                            <Grid item xs={12} sm={6}>
-                                                <Card>
-                                                    <CardContent>
-                                                        <Typography variant="h6" component="div">
-                                                            <Star /> Career High Rank: {playerData.career_high_rank}
-                                                        </Typography>
-                                                        <Typography variant="body2" color="text.secondary">
-                                                            <CalendarToday /> Career High Year: {playerData.career_high_year}
-                                                        </Typography>
-                                                    </CardContent>
-                                                </Card>
-                                            </Grid>
-                                        </Grid>
+                                        <br />
                                         <Divider />
-                                        Rank Level: {playerData.rank_level} <br />
-                                        Status: {playerData.status} <br />
-                                        Experience: {playerData.experience} <br />
-                                        Age: {playerData.age} <br />
-                                        Height: {playerData.height} <br />
-                                        Favorite Shot: {playerData.favorite_shot} <br />
-                                        Hand: {playerData.hand} <br />
-                                        Grass Advantage: {playerData.grass_advantage} <br />
-                                        Years on Tour: {playerData.years_on_tour} <br />
-                                        Coach: {playerData.coach} <br />
-                                        Gender: {playerData.gender} <br />
-                                    </CardContent>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <CardContent>
-                                        <Typography variant="h5" component="div">
-                                            Personality Tags
-                                        </Typography>
-                                        <Box sx={{ display: 'flex', flexWrap: 'wrap', '& > :not(style)': { m: 0.5 } }}>
-                                            {playerData.personality_tags.map((tag) => (
-                                                <Chip label={tag} key={tag} />
-                                            ))}
+                                        <br />
+
+                                        <Box paddingLeft={2}>
+                                            <Typography variant="body1" color="text.secondary">
+                                                {playerData.personality_long}
+                                            </Typography>
                                         </Box>
+                                        <Button variant="body2" onClick={handleExpandClick} endIcon={<ExpandMore />} >
+                                            More info
+                                        </Button>
+
+                                        <Collapse in={expanded} timeout="auto" unmountOnExit>
+                                            <CardContent>
+                                                <Typography variant="h5" component="div">
+                                                    Numeric Stats
+                                                </Typography>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Age: {playerData.age} <br />
+                                                    Years on Tour: {playerData.years_on_tour} <br />
+                                                    Height: {playerData.height} <br /><br />
+                                                </Typography>
+                                                <Typography variant="h5" component="div">
+                                                    Categorical Stats
+                                                </Typography>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    {playerData.status && <span>Status: {playerData.status} <br /></span>}
+                                                    Favorite Shot: {playerData.favorite_shot} <br />
+                                                    Hand: {playerData.hand} <br />
+                                                    Grass Advantage: {playerData.grass_advantage} <br />
+                                                    Coach: {playerData.coach} <br />
+                                                </Typography>
+                                            </CardContent>
+                                        </Collapse>
+
                                     </CardContent>
-                                </Grid>
-                            </Grid>
+                                </CardContent>
+
+                                <Box paddingLeft={2}>
+                                    <Grid item xs={12}>
+                                        <CardContent>
+                                            <Typography variant="h5" component="div">
+                                                Personality Tags
+                                            </Typography>
+                                            <Box sx={{ display: 'flex', flexWrap: 'wrap', '& > :not(style)': { m: 0.5 } }}>
+                                                {playerData.personality_tags.map((tag) => (
+                                                    <Chip label={tag} key={tag} />
+                                                ))}
+                                            </Box>
+                                        </CardContent>
+                                    </Grid>
+                                </Box>
+                            </Card>
                         )
                     )}
                 </Card>
--- frontend/src/components/TriangleSlider.jsx
+++ frontend/src/components/TriangleSlider.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Tooltip } from 'react-tooltip';
+import { Button, Card } from '@mui/material';
 
 const TriangleSlider = () => {
   const points = [
@@ -16,45 +17,59 @@ const TriangleSlider = () => {
   const [personalityWeight, setPersonalityWeight] = useState(0.33);
   const [numericWeight, setNumericWeight] = useState(0.33);
   const [categoricalWeight, setCategoricalWeight] = useState(0.33);
+  const [weightDistribution, setWeightDistribution] = useState('all');
 
   const handleClick = (id) => {
     setSelectedPoint(id);
     setPersonalityWeight(id === 1 ? 1 : id === 2 ? 0.5 : id === 4 ? 0.5 : id === 5 ? 0.33 : 0);
     setNumericWeight(id === 2 ? 0.5 : id === 3 ? 1 : id === 5 ? 0.33 : id === 6 ? 0.5 : 0);
     setCategoricalWeight(id === 4 ? 0.5 : id === 5 ? 0.33 : id === 6 ? 0.5 : id === 7 ? 1 : 0);
+    setWeightDistribution(id === 1 ? 'tag_similarity' : id === 2 ? 'tag_numeric' : id === 3 ? 'numeric' :
+      id === 4 ? 'tag_categorical' : id === 5 ? 'all' : id === 6 ? 'numeric_categorical' : 'categorical')
   };
+
+  const handleWeightChange = (event) => {
+    // Send the new weightDistribution to the backend
+    console.log(weightDistribution)
+  }
 
   return (
     <div>
-      <p>Use these dots to select how much each type of data point affects the similarity. Hover over each dot to see how it weighs the different types. The following are descriptions for each of the types.</p>
-      <ul>
-        <li>Personality Tags: These are tags that describe a person's personality. For example, "funny", "outgoing", "shy", etc. See them at the bottom of each player once clicked.</li>
-        <li>Numeric: These are numbers that describe a person. For example, "height", "weight", "age", etc. See them by clicking a player.</li>
-        <li>Categorical: These are other notable attributes, such as a person's "handedness", "favorite shot", etc. See them by clicking a player.</li>
-      </ul>
-      <p>Similarity function:</p>
-      <p>similarity = {personalityWeight} * personalitySimilarity + {numericWeight} * numericSimilarity + {categoricalWeight} * categoricalSimilarity</p>
+      <Card>
+        <p>Use these dots to select how much each type of data point affects the similarity. Hover over each dot to see how it weighs the different types. The following are descriptions for each of the types.</p>
+        <ul>
+          <li>Personality Tags: These are tags that describe a person's personality. For example, "funny", "outgoing", "shy", etc. See them at the bottom of each player once clicked.</li>
+          <li>Numeric: These are numbers that describe a person. For example, "height", "weight", "age", etc. See them by clicking a player.</li>
+          <li>Categorical: These are other notable attributes, such as a person's "handedness", "favorite shot", etc. See them by clicking a player.</li>
+        </ul>
+        <p>Similarity function ({weightDistribution}):</p>
+        <p>similarity = {personalityWeight} * personalitySimilarity + {numericWeight} * numericSimilarity + {categoricalWeight} * categoricalSimilarity</p>
 
-      <svg width="200" height="140">
-        <g transform="translate(10, 10)">
-          {points.map((point) => (
-            <circle
-              data-tooltip-id="my-tooltip"
-              data-tooltip-content={`Point ${point.id}`}
-              key={point.id}
-              cx={point.x}
-              cy={point.y}
-              r="5"
-              fill={selectedPoint === point.id ? 'red' : 'blue'}
-              onClick={() => handleClick(point.id)}
-            />
-          ))}
-          <text x="0" y="0" fontSize="10">Personality Tags</text>
-          <text x="120" y="0" fontSize="10">Numeric</text>
-          <text x="65" y="115" fontSize="10">Categorical</text>
-        </g>
-      </svg>
-      <Tooltip id='my-tooltip'/>
+        <svg width="200" height="140">
+          <g transform="translate(10, 10)">
+            {points.map((point) => (
+              <circle
+                data-tooltip-id="my-tooltip"
+                data-tooltip-content={`Point ${point.id}`}
+                key={point.id}
+                cx={point.x}
+                cy={point.y}
+                r="5"
+                fill={selectedPoint === point.id ? 'red' : 'blue'}
+                onClick={() => handleClick(point.id)}
+              />
+            ))}
+            <text x="0" y="0" fontSize="10">Personality Tags</text>
+            <text x="120" y="0" fontSize="10">Numeric</text>
+            <text x="65" y="115" fontSize="10">Categorical</text>
+          </g>
+        </svg>
+        <Tooltip id='my-tooltip' />
+        {/* Add an "Update" button which sends this to the backend for player similarity weight */}
+        <Button onClick={() => handleWeightChange()}>
+          Update weight
+        </Button>
+      </Card>
     </div>
   );
 };

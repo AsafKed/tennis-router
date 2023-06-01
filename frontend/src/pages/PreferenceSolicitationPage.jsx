c1d687d0d98df9e0009604cc28107497a3c8823c
AsafKed <asafkedem97@gmail.com>
2023-06-01 14:53:35 +0200

    Init discrete slider for preference strength selection

--- frontend/src/pages/PreferenceSolicitationPage.jsx
+++ frontend/src/pages/PreferenceSolicitationPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PlayerSelectionPage from '../components/PlayerSelection';
 import UserPreferences from "../components/UserPreferences";
-
+import DiscreteSlider from "../components/DiscreteSlider";
 
 const PreferenceSolicitationPage = () => {
     // Have a toggle to show either the player selection page or the user preferences page
@@ -16,6 +16,7 @@ const PreferenceSolicitationPage = () => {
             <h1>Preference Solicitation</h1>
             <button onClick={handleToggle}>Toggle</button>
             {showPlayerSelectionPage ? <PlayerSelectionPage /> : <UserPreferences />}
+            <DiscreteSlider />
         </div>
     )
 }

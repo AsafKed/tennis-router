b2b639f06fb1815cf7a1da1dd34a4f75be3c753b
AsafKed <asafkedem97@gmail.com>
2023-06-04 12:50:48 +0200

    Track login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,41 +1,54 @@
-import React, {useState} from 'react';
-import {  signInWithEmailAndPassword   } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
 import { NavLink, useNavigate } from 'react-router-dom'
 import { Alert } from '@mui/material';
- 
+
+// Tracking
+import { dispatchTrackingData } from '../TrackingDispatcher';
+import { track, useTracking } from 'react-tracking';
+
 const Login = () => {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState(null)
-       
+
+    const { trackEvent } = useTracking();
+
+    // Upon opening the page
+    useEffect(() => {
+        trackEvent({ action: 'page_open' })
+    }, []);
+
     const onLogin = (e) => {
         e.preventDefault();
         signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            navigate("/user")
-            console.log(user);
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            setError(errorMessage)
-            console.log(errorCode, errorMessage)
-        });
-       
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                trackEvent({ action: 'login' });
+                navigate("/user")
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                setError(errorMessage)
+                trackEvent({ action: 'login_error', 'error': errorCode })
+                console.log(errorCode, errorMessage)
+            });
+
     }
- 
-    return(
+
+    return (
         <>
-            <main >        
+            <main >
                 <section>
-                    <div>                                            
-                        <p> FocusApp </p>                       
-                                                       
-                        <form>                                              
+                    <div>
+                        <p> FocusApp </p>
+
+                        <form>
                             <div>
                                 <label htmlFor="email-address">
                                     Email address
@@ -43,10 +56,10 @@ const Login = () => {
                                 <input
                                     id="email-address"
                                     name="email"
-                                    type="email"                                    
-                                    required                                                                                
+                                    type="email"
+                                    required
                                     placeholder="Email address"
-                                    onChange={(e)=>setEmail(e.target.value)}
+                                    onChange={(e) => setEmail(e.target.value)}
                                 />
                             </div>
 
@@ -57,35 +70,35 @@ const Login = () => {
                                 <input
                                     id="password"
                                     name="password"
-                                    type="password"                                    
-                                    required                                                                                
+                                    type="password"
+                                    required
                                     placeholder="Password"
-                                    onChange={(e)=>setPassword(e.target.value)}
+                                    onChange={(e) => setPassword(e.target.value)}
                                 />
                             </div>
-                                                
+
                             <div>
-                                <button                                    
-                                    onClick={onLogin}                                        
-                                >      
-                                    Login                                                                  
+                                <button
+                                    onClick={onLogin}
+                                >
+                                    Login
                                 </button>
                                 {error && <Alert severity="error">{error}</Alert>}
-                            </div>                               
+                            </div>
                         </form>
-                       
+
                         <p className="text-sm text-white text-center">
                             No account yet? {' '}
                             <NavLink to="/register">
                                 Sign up
                             </NavLink>
                         </p>
-                                                   
+
                     </div>
                 </section>
             </main>
         </>
     )
 }
- 
-export default Login
+
+export default track({ page: 'Login' }, { dispatch: dispatchTrackingData })(Login)

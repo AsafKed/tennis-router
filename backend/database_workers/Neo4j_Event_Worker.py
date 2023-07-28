8a9d7fc8a0885998217637a236e3466cb33d3b78
AsafKed <asafkedem97@gmail.com>
2023-07-29 00:03:55 +0200

    Upload jupyter notebook with data exploration

--- backend/database_workers/Neo4j_Event_Worker.py
+++ backend/database_workers/Neo4j_Event_Worker.py
@@ -58,3 +58,87 @@ class Event_Worker:
         result = tx.run(query, node_props=node_props, rel_props=rel_props, user_id=user_id, guest_id=guest_id)
 
         return result.single()
+
+
+    #############################
+    # Return timestamps
+    #############################
+    def get_view_player_timestamps(self):
+        with self.driver.session(database="neo4j") as session:
+            result = session.run("""
+                MATCH (e:Event {action: "view_player"})-[r]-(n) 
+                RETURN r.timestamp as timestamp
+            """)
+            return [record["timestamp"] for record in result]
+
+    def get_player_view_open_timestamps(self):
+        with self.driver.session(database="neo4j") as session:
+            result = session.run("""
+                MATCH (e:Event {action: "player_view_open"})-[r]-(n) 
+                RETURN r.timestamp as timestamp
+            """)
+            return [record["timestamp"] for record in result]
+        
+    
+    #############################
+    # Count view player relations
+    #############################
+    def count_view_player(self):
+        with self.driver.session(database="neo4j") as session:
+            result = session.run("""
+                MATCH (e:Event {action: "view_player"})-[r]-(n) 
+                WHERE n.name <> "Asaf"
+                RETURN r.player_name as player_name, e.page as page, count(*) as count
+            """)
+
+            # Convert the result into a nested dictionary
+            player_counts = {}
+            for record in result:
+                player_name = record["player_name"].replace('_', ' ')
+                page = record["page"]
+                count = record["count"]
+
+                if player_name not in player_counts:
+                    player_counts[player_name] = {"total": 0}
+                if page not in player_counts[player_name]:
+                    player_counts[player_name][page] = 0
+
+                player_counts[player_name]["total"] += count
+                player_counts[player_name][page] += count
+
+            # Sort the dictionary by total count in descending order
+            sorted_player_counts = dict(sorted(player_counts.items(), key=lambda item: item[1]["total"], reverse=True))
+            
+            return sorted_player_counts
+        
+
+    #############################
+    # Count liked players
+    #############################
+    def count_like_player(self):
+        with self.driver.session(database="neo4j") as session:
+            result = session.run("""
+                MATCH (e:Event {action: "like_player"})-[r]-(n) 
+                WHERE n.name <> "Asaf"
+                RETURN r.player_name as player_name, e.page as page, count(*) as count
+            """)
+
+            # Convert the result into a nested dictionary
+            player_counts = {}
+            for record in result:
+                player_name = record["player_name"].replace('_', ' ')
+                page = record["page"]
+                count = record["count"]
+
+                if player_name not in player_counts:
+                    player_counts[player_name] = {"total": 0}
+                if page not in player_counts[player_name]:
+                    player_counts[player_name][page] = 0
+
+                player_counts[player_name]["total"] += count
+                player_counts[player_name][page] += count
+
+            # Sort the dictionary by total count in descending order
+            sorted_player_counts = dict(sorted(player_counts.items(), key=lambda item: item[1]["total"], reverse=True))
+            
+            return sorted_player_counts
